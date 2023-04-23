88183b5f2f100fc93230a8fa2aad8ffe2a43b5f5
Muhammad Yasir <64388216+yasirsardar99@users.noreply.github.com>
2023-04-23 09:50:11 +0500

    seventh commit

--- src/components/News.js
+++ src/components/News.js
@@ -269,16 +269,50 @@ export class News extends Component {
     console.log("checking constructor working ")
     this.state = {
       articles: this.articles,
-      loading: true
+      loading:false,
+      page: 1,
+    
+
     }
   }
 
   async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6e4777699b4945c4823a222fce23cade";
+    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6e4777699b4945c4823a222fce23cade&page=1&pagesize=12";
+    let data =  await fetch(url);
+    let parsedData = await data.json()
+    console.log("parsed data")
+    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+
+  }
+  handlePrevClick = async ()=>{
+   
+    console.log("previous")
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6e4777699b4945c4823a222fce23cade&page=${this.state.page-1}&pagesize=12`;
+    let data =  await fetch(url);
+    let parsedData = await data.json()
+    this.setState({
+        page: this.state.page -1,
+        articles: parsedData.articles
+      }
+      )
+  }
+
+  handleNextClick = async()=>{
+    console.log("next")
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/12))
+    {
+        
+    }
+    else{
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6e4777699b4945c4823a222fce23cade&page=${this.state.page + 1}&pagesize=12`;
     let data =  await fetch(url);
     let parsedData = await data.json();
-    this.setState({articles: parsedData.articles})
-
+    this.setState(
+      {articles: parsedData.articles, 
+        page: this.state.page +1
+      }
+      )
+    }
   }
   render() {
     return (
@@ -289,11 +323,15 @@ export class News extends Component {
         <div className="row">
         {this.state.articles.map((element)=>{
             return  <div className="col-md-4" key={element.url}>
-            <NewsItem  title={element.title?element.title.slice(0,45):" "} description={element.description?element.description.slice(0,88):" "} imgUrl={element.urlToImage} newsUrl={element.url}/>
+            <NewsItem  title={element.title?element.title:" "} description={element.description?element.description:" "} imgUrl={element.urlToImage} newsUrl={element.url}/>
             
             </div>
         })}
            
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button"  className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button  disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/12)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     </>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div>
       <div className="card my-2 " style={{width: "18rem"}}>
-  <img src={!imgUrl?"https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg" :imgUrl} className="card-img-top" alt="..."/>
+  <img src={imgUrl?imgUrl :"https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg"} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
-    <a href={newsUrl} className="btn btn-primary" >click to Read</a>
+    <a href={newsUrl} className="btn btn-light border border-dark border-3 rounded d-flex justify-content-center" >click to Read</a>
   </div>
 </div>
       </div>
